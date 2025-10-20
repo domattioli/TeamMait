9332e763e7fe92c7d7f9c072c83336efce2905e6
domattioli <iqh766c2@duck.com>
2025-10-20 14:49:52 -0500

    Move disclaimers to bottom

--- pages/1_Open Chat.py
+++ pages/1_Open Chat.py
@@ -332,8 +332,8 @@ st.markdown(
 
 st.markdown("<div class='app-container'>", unsafe_allow_html=True)
 st.title("TeamMait Private Conversation")
-st.markdown("<p style='font-size:12px;color:#6b7280;margin-top:6px;'>Disclaimer: TeamMait may be incorrect or incomplete. Please verify information..</p>", unsafe_allow_html=True)
 st.markdown("<p style='font-size:12px;color:#e11d48;margin-top:6px;'><strong>Privacy Reminder:</strong> Please do not include any identifying information in your messages.</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size:12px;color:#6b7280;margin-bottom:6px;'>Disclaimer: TeamMait may be incorrect or incomplete. Please verify information..</p>", unsafe_allow_html=True)
 
 
 # # ---------- Load JSON Conversation into Vector Store ----------
